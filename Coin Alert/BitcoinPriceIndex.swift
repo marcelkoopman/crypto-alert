42b12ddb74f972d2dbc3c29ded7a9f2c9410bab3
marcelkoopman <marcel.koopman@gmail.com>
2019-05-04 14:17:37 +0200

    Initial commit

---
+++ Coin Alert/BitcoinPriceIndex.swift
@@ -0,0 +1,13 @@
+//
+//  CoinDeskInfo.swift
+//  Coin Alert
+//
+//  Created by Marcel Koopman on 04/05/2019.
+//  Copyright © 2019 Marcel Koopman. All rights reserved.
+//
+
+import Foundation
+
+class BitcoinPriceIndex: Codable {
+    
+}
